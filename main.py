57ab26538e7cabac19f7c3a959dbd7ee8cdb4e7c
Rares-Patrascu <rares926@gmail.com>
2021-08-06 15:57:04 +0300

    Implemented a method for splitting and moving data in the train/test folders

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Pattern
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,6 +7,10 @@ import io
 import itertools
 import sklearn.metrics
 import os
+import shutil
+import fnmatch
+
+animalsDataset='C:/animalsDataset'
 
 def createFolders(root):
     print(root)
@@ -19,5 +24,31 @@ def createFolders(root):
             pass
         print("Created %s directory" %path)
 
+
+def splitData():
+
+    quotient=4/5
+    directories=('cats','dogs')
+
+    for i in range(2):
+        list = os.listdir(os.path.join(animalsDataset,directories[i]))
+        numberOfFiles=len(list)
+        # toBeTrained=int(quotient*numberOfFiles)
+        # toBeTested=numberOfFiles-toBeTrained
+        toBeTrained=100
+        numberOfFiles=150
+
+        for photo in range(toBeTrained):
+            source=os.path.join(animalsDataset,directories[i],list[photo])
+            destination='splittedData/train'
+            shutil.copy(source,destination)
+
+        for photo in range(toBeTrained,numberOfFiles):
+            source=os.path.join(animalsDataset,directories[i],list[photo])
+            destination='splittedData/test'
+            shutil.copy(source,destination)
+
+
 if __name__ == "__main__":
     createFolders(os.getcwd())
+    splitData()
