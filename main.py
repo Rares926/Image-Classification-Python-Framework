8b0171d1023b0a0c11ff822fb674b0fa5103f9cd
Radu-Baciu <radubaciu218@yahoo.com>
2021-08-06 14:28:23 +0300

    Automatically create train and test folders.

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ import datetime
 import io
 import itertools
 import sklearn.metrics
+import os
+
+def createFolders(root):
+    directories = ('train', 'test')
+    for i in range(2):
+        path = os.path.join(root, directories[i])
+        os.mkdir(path)
+        print("Created %s directory" %path)
+
+if __name__ == "__main__":
+    createFolders("C:/Users/Radu Baciu/Desktop/asdf")
