df7af80b678c3c7b6e8563c2a063c32f88a3c830
Radu-Baciu <radubaciu218@yahoo.com>
2021-08-11 13:45:02 +0300

    Revert "Merge branch 'develop' into main"
    This reverts commit 08d3a3d5be0166a50834bd3d44d9c308ffce32ca, reversing
    changes made to 62003a7285c58508775b39be614ad5edc2a62079.

--- core/train_worker.py
+++ core/train_worker.py
@@ -1,22 +1,18 @@
 import tensorflow as tf
+import datetime
+import os
 
 # Internal framework imports
 
+from utils.io_helper import IOHelper
 # Typing imports imports
 
 
 class TrainWorker:
-    def __init__(self, model_layers, augment_layers = None):
-        self.model = model_layers
-        self.augments = augment_layers
-
+    def __init__(self):
+        self.model = None
+    
     def create_model(self, labels):
-        # self.model = tf.keras.models.Sequential([
-        #     self.augments,
-        #     self.model,
-        #     tf.keras.layers.Dense(len(labels), activation='softmax')
-        # ])
-
         self.model = tf.keras.models.Sequential([
             tf.keras.layers.Conv2D(16, (3, 3), padding='same', activation='relu', input_shape=(224, 224, 3)),
             tf.keras.layers.BatchNormalization(),
@@ -40,6 +36,8 @@ class TrainWorker:
             tf.keras.layers.Dense(len(labels), activation='softmax')
         ])
 
+        self.model.summary()
+
         loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = False)
 
         optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
@@ -48,13 +46,12 @@ class TrainWorker:
                 loss=loss_fn,
                 metrics=['accuracy'])
 
+
     def train(self, workspace, x_train, y_train, x_test, y_test, epochs = 10):
-<<<<<<< Updated upstream
+
         if self.model is None:
             raise Exception("The model must be created in order to be used!")
 
-        self.model.fit(x_train, y_train, epochs=epochs)
-=======
 
         checkpoint_path = os.path.join(workspace,"checkpoints")
         IOHelper.create_directory(checkpoint_path)
@@ -73,8 +70,8 @@ class TrainWorker:
 
 
         self.model.fit(x_train, y_train, epochs=epochs, callbacks=[tensorboard_callback, cp_callback])
->>>>>>> Stashed changes
 
         test_loss, test_acc = self.model.evaluate(x_test, y_test, verbose=1)
+        
         print('\nTest loss:', test_loss)
         print('\nTest accuracy:', test_acc)
