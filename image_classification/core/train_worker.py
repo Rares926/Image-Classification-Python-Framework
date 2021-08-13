b0ee07fe6ee693857853a919faad3aa2f2da0d69
Rares-Patrascu <rares926@gmail.com>
2021-08-13 16:18:30 +0300

    Changed the folders architecture

--- image_classification/core/train_worker.py
+++ image_classification/core/train_worker.py
@@ -13,16 +13,9 @@ class TrainWorker:
         self.model=model
 
     def train(self, workspace:str, x_train:np.ndarray, y_train:np.array, x_test:np.ndarray, y_test:np.array, epochs:int = 10):
-        loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = False)
-
-        optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-
-        self.model.compile(optimizer=optimizer,
-                    loss=loss_fn,
-                    metrics=['accuracy'])
-                    
         if self.model is None:
             raise Exception("The model must be created in order to be used!")
+
 
         checkpoint_path = os.path.join(workspace,"checkpoints")
         IOHelper.create_directory(checkpoint_path)
@@ -37,6 +30,15 @@ class TrainWorker:
         workspace=workspace+"/tensorboard/"+datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
         tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=workspace, histogram_freq=1)
 
+        
+        loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = False)
+
+        optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+
+        self.model.compile(optimizer=optimizer,
+                    loss=loss_fn,
+                    metrics=['accuracy'])
+                    
         self.model.fit(x_train, y_train, epochs=epochs, callbacks=[tensorboard_callback, cp_callback])
 
         test_loss, test_acc = self.model.evaluate(x_test, y_test, verbose=1)
