655f752af5fa3273a59f4925fc074a3ef7c81b90
Rares-Patrascu <rares926@gmail.com>
2021-08-17 19:14:50 +0300

    Image processing class with strech and aspect-ratio resize

--- image_classification/image_classification/core/image_preprocessing.py
+++ image_classification/image_classification/core/image_preprocessing.py
@@ -1,31 +1,82 @@
 import cv2 as cv
-import os
+import numpy as np
 # Internal framework imports
 
 # Typing imports imports
 
-class ImagePreprocessing:
+class ImageProcessing:
 
     def __init__(self):
         pass
 
     @staticmethod
-    def strech(image,size):
-        return cv.resize(image, (size, size))
+    def strech(image,height,width=0):
+
+        if image is None:
+            raise("Image is empty")
+
+        old_height, old_width = image.shape[:2]
+        
+        if old_height==height and old_width==width:
+            return image
+        elif width==0:
+            return cv.resize(image, (height, height))
+        else:
+            return cv.resize(image, (height, width))
 
     @staticmethod
-    def crop():
-        print("c")
+    def letterbox(image,height,width,inter = cv.INTER_AREA):
+
+        (h, w) = image.shape[:2]
+
+        aspect=min(height/h,width/w)
+
+        new_dim=(int(aspect*w),int(aspect*h))
+
+        resized = cv.resize(image, new_dim, interpolation = inter)
+
+        return resized
 
     @staticmethod
-    def letterbox():
-        print("l")
+    def aspect_ratio_resize(image, width = None, height = None, inter = cv.INTER_AREA):
+
+        dim = None
+        (h, w) = image.shape[:2]
+
+        if width is None and height is None:
+            return image
+
+        if width is None:
+            r = height / float(h)
+            dim = (int(w * r), height)
+
+        else:
+            r = width / float(w)
+            dim = (width, int(h * r))
+
+        resized = cv.resize(image, dim, interpolation = inter)
+
+        # return the resized image
+        return resized
+
+
+
+
+
 
 
 def run():
-    img_arr = cv.imread("C:/Users/test/Desktop")
-    cv.imshow("img_arr")
-    # cv.imshow()
+    # C:\Users\test\Desktop
+    img = cv.imread("C:/Users/test/Desktop/550&320.png")
+    cv.imshow('Original image', img)
+    cv.waitKey(0)
+    # height, width = img.shape[:2] # shape ul trebuie salvbat in alta clasa in functie de tipul imaginii rgb ,greyscale etc 
+    # img=ImageProcessing.strech(img,800,200)
+    # img = ImageProcessing.aspect_ratio_resize(img,700)
+    img=ImageProcessing.letterbox(img,300,500)
+    # print(img)
+    cv.imshow('Processed image', img)
+    cv.waitKey(0)
 
 
 
