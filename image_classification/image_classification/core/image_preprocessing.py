824d182db24f5d1d9826c8027526fccc3af91ca3
Rares-Patrascu <rares926@gmail.com>
2021-08-18 19:27:56 +0300

    Implemented letterbox,crop,aspect_ratio_resize and border methods for image processing

--- image_classification/image_classification/core/image_preprocessing.py
+++ image_classification/image_classification/core/image_preprocessing.py
@@ -1,31 +1,47 @@
 import cv2 as cv
-import numpy as np
-# Internal framework imports
-
-# Typing imports imports
+import matplotlib.pyplot as plt 
+from PIL import Image
 
 class ImageProcessing:
 
-    def __init__(self):
-        pass
-
     @staticmethod
-    def strech(image,height,width=0):
+    def strech(image, dimension = None, height = None, width = None):
 
         if image is None:
             raise("Image is empty")
+        elif dimension is None:
 
-        old_height, old_width = image.shape[:2]
-        
-        if old_height==height and old_width==width:
-            return image
-        elif width==0:
-            return cv.resize(image, (height, height))
+            old_height, old_width = image.shape[:2]
+
+            if old_height==height and old_width==width:
+                return image
+            else:
+                return cv.resize(image, (width, height))
         else:
-            return cv.resize(image, (height, width))
+            return cv.resize(image, (dimension, dimension))
 
     @staticmethod
-    def letterbox(image,height,width,inter = cv.INTER_AREA):
+    def border(image ,height ,width ,padding = "center"):
+
+        (h, w) = image.shape[:2]
+
+        if padding=="center":
+
+                top=bottom=int((height-h)/2)
+                left=right=int((width-w)/2)
+
+                image = cv.copyMakeBorder(image,top, bottom, left, right, cv.BORDER_CONSTANT, None, value = 0)
+                image=cv.resize(image,(width,height))
+
+        elif padding=="top-left":
+                bottom=height-h
+                right=width-w
+                image = cv.copyMakeBorder(image,0, bottom, 0, right, cv.BORDER_CONSTANT, None, value = 0)
+
+        return image
+
+    @staticmethod
+    def aspect_ratio_resize(image,height=None,width=None,inter = cv.INTER_AREA):
 
         (h, w) = image.shape[:2]
 
@@ -38,49 +54,49 @@ class ImageProcessing:
         return resized
 
     @staticmethod
-    def aspect_ratio_resize(image, width = None, height = None, inter = cv.INTER_AREA):
-
-        dim = None
-        (h, w) = image.shape[:2]
-
-        if width is None and height is None:
+    def letterbox(image,dimension=None,height=None,width=None,padding='center'):
+        
+        if image is None:
+            raise("Image is empty")
+        elif dimension != None:
+            image=ImageProcessing.aspect_ratio_resize(image,dimension,dimension)
+        elif height is None and width is None:
             return image
-
-        if width is None:
-            r = height / float(h)
-            dim = (int(w * r), height)
-
         else:
-            r = width / float(w)
-            dim = (width, int(h * r))
-
-        resized = cv.resize(image, dim, interpolation = inter)
-
-        # return the resized image
-        return resized
+             image=ImageProcessing.aspect_ratio_resize(image,height,width)
 
 
+        bordered_image=ImageProcessing.border(image,height,width,padding)
+        return bordered_image
 
+    @staticmethod
+    def crop(image, x_axis = None, y_axis = None, height = None, width = None, n_height = None, n_width = None):
 
+        crop = image[y_axis:y_axis+height, x_axis:x_axis+width]
+        
+        crop=ImageProcessing.letterbox(crop,height=n_height,width=n_width,padding="top-left")
 
+        return crop
+        
 
 
 def run():
-    # C:\Users\test\Desktop
     img = cv.imread("C:/Users/test/Desktop/550&320.png")
+
     cv.imshow('Original image', img)
     cv.waitKey(0)
-    # height, width = img.shape[:2] # shape ul trebuie salvbat in alta clasa in functie de tipul imaginii rgb ,greyscale etc 
-    # img=ImageProcessing.strech(img,800,200)
-    # img = ImageProcessing.aspect_ratio_resize(img,700)
-    img=ImageProcessing.letterbox(img,300,500)
-    # print(img)
-    cv.imshow('Processed image', img)
+
+    img1=ImageProcessing.letterbox(img,height=500,width=700,padding="center")
+    cv.imshow('Processed image', img1)
     cv.waitKey(0)
 
+    img2=ImageProcessing.strech(img,height=300,width=400)
+    cv.imshow("Streched image",img2)
+    cv.waitKey(0)
 
-
-
+    img3=ImageProcessing.crop(img,100,100,300,300,320,550)
+    cv.imshow("Cropped image",img3)
+    cv.waitKey(0)
 
 
 if __name__ == "__main__":
--- image_classification/image_classification/core/test_worker.py
+++ image_classification/image_classification/core/test_worker.py
@@ -3,6 +3,7 @@ import cv2 as cv
 import numpy as np
 import os
 
+
 # Internal framework imports
 from .data_visualization import DataVisualization
 # Typing imports imports
