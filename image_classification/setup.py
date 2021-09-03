82d945e36e8fea5a8d12259de26a9bc8257dcbfb
Radu_Baciu_ <radubaciu218@yahoo.com>
2021-09-03 10:18:08 +0300

    Updated requirements in setup.py

--- image_classification/setup.py
+++ image_classification/setup.py
@@ -13,8 +13,10 @@ setup(
             'opencv-python==4.5.3.56',
             'tensorflow==2.5.0',
             'seaborn==0.11.1',
+            'scikit-learn==0.24.2',
             'jsonargparse==3.4.1',
-            'albumentations==1.0.3'
+            'albumentations==1.0.3',
+            'tensorflow-hub==0.12.0'
       ],
       entry_points={
             'console_scripts': [
